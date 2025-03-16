010b4eb1f1c7bd9dd95e902520614929ee39a84f
FittLife <fittlifecorp@gmail.com>
2025-03-16 15:36:50 +0800

    Update form_handler.php

--- form_handler.php
+++ form_handler.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$email_from = 'zayiiiieeee@gmail.com'
+$email_from = 'fittlife.github.io'
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
